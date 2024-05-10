5f053500b32272663a8cbb4f7ef5a86898759c2a
Agustin <agustin.ledezma@ucb.edu.bo>
2024-05-10 15:37:15 -0400

    Se refactorizo separando en funcionalidades mas sencillas para que sea mas entendible el codigo

--- src/calculadorSumaDeCadena.js
+++ src/calculadorSumaDeCadena.js
@@ -1,18 +1,28 @@
-function calculadorSumaDeCadena(cadena) {
-  var elementos = cadena.split(" ");
-  var resultado = 0;
+function separarCadena(cadena) {
   if (!cadena) {
-    return "No se puso nada en la cadena";
+      return [];
   }
-  for(var iterador = 0; iterador < elementos.length; iterador++){
-    if(!isNaN(elementos[iterador])){
-      resultado += parseInt(elementos[iterador]);
-    }else{
-      return "Elemento no valido " + elementos[iterador];
-    }
-    
+  return cadena.split(" ");
+}
+
+function sumarElementosdeCadena(elementos) {
+  var resultado = 0;
+  for (var iterador = 0; iterador < elementos.length; iterador++) {
+      if (!isNaN(elementos[iterador])) {
+          resultado += parseInt(elementos[iterador]);
+      } else {
+          return "Elemento no valido " + elementos[iterador];
+      }
   }
   return resultado;
+}
+
+function calculadorSumaDeCadena(cadena) {
+  var elementos = separarCadena(cadena);
+  if (elementos.length === 0) {
+      return "No se puso nada en la cadena";
+  }
+  return sumarElementosdeCadena(elementos);
 }
 
 export default calculadorSumaDeCadena;
