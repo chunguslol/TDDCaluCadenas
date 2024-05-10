1b2823276585803607c6575351684bf9a5ae7015
Agustin <agustin.ledezma@ucb.edu.bo>
2024-05-10 15:26:59 -0400

    Deberia delvover la suma de la cadena

--- src/calculadorSumaDeCadena.test.js
+++ src/calculadorSumaDeCadena.test.js
@@ -4,7 +4,7 @@ describe("calculadorSumaDeCadena", () => {
   it("Deberia retornar nada cuando no se ingresa nada en el calculadorSumaDeCadena", () => {
     expect(calculadorSumaDeCadena("")).toEqual("No se puso nada en la cadena");
   });
-  it("Deberia concatenar el resultado todo junto", () => {
-    expect(calculadorSumaDeCadena("10 20")).toEqual("01020");
+  it("Deberia devolver la suma de la cadena", () => {
+    expect(calculadorSumaDeCadena("10 20")).toEqual(30);
   });
 });
