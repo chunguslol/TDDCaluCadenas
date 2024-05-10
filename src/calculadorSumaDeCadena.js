16ac3ae40c1011bf3b5f29ad4d6a5049463281c3
Agustin <agustin.ledezma@ucb.edu.bo>
2024-05-10 15:22:39 -0400

    Deberia concatenar el resultado todo junto

--- src/calculadorSumaDeCadena.js
+++ src/calculadorSumaDeCadena.js
@@ -1,7 +1,13 @@
 function calculadorSumaDeCadena(cadena) {
+  var el = cadena.split(" ");
+  var resul = 0;
   if (!cadena) {
     return "No se puso nada en la cadena";
   }
+  for(var i = 0; i < el.length; i++){
+    resul = resul + el[i];
+  }
+  return resul;
 }
 
 export default calculadorSumaDeCadena;
--- src/calculadorSumaDeCadena.test.js
+++ src/calculadorSumaDeCadena.test.js
@@ -4,4 +4,7 @@ describe("calculadorSumaDeCadena", () => {
   it("Deberia retornar nada cuando no se ingresa nada en el calculadorSumaDeCadena", () => {
     expect(calculadorSumaDeCadena("")).toEqual("No se puso nada en la cadena");
   });
+  it("Deberia concatenar el resultado todo junto", () => {
+    expect(calculadorSumaDeCadena("10 20")).toEqual("01020");
+  });
 });
