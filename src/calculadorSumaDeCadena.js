35a15ec228329a902f488dd281834b6ef5e1771f
Agustin <agustin.ledezma@ucb.edu.bo>
2024-05-10 22:36:39 -0400

    Se refactorizo haciendo mas funciones simples para que cada uno haga una cosa y sea mas legible el codigo, ademas de corregir un error del primer comit que al no tener nada que devuelva 0

--- src/calculadorSumaDeCadena.js
+++ src/calculadorSumaDeCadena.js
@@ -1,36 +1,51 @@
-function separarCadena(cadena) {
+function obtenerDelimitador(cadena) {
   const coma = ",";
-  if (!cadena) {
-      return [];
-  }
   var delimitador = coma;
   if (cadena.startsWith("//")) {
       var inicioDelimitador = cadena.indexOf("[") + 1;
       var finDelimitador = cadena.indexOf("]");
       delimitador = cadena.substring(inicioDelimitador, finDelimitador);
-      cadena = cadena.substring(finDelimitador + 2); 
   }
-  return cadena.split(new RegExp("[,|-]" + "|" + delimitador));
+  return delimitador;
+}
+
+function obtenerCadenaSinDelimitador(cadena) {
+  if (cadena.startsWith("//")) {
+      var finDelimitador = cadena.indexOf("]") + 2;
+      return cadena.substring(finDelimitador); 
+  }
+  return cadena;
+}
+
+function separarCadena(cadena) {
+  const delimitador = obtenerDelimitador(cadena);
+  const cadenaSinDelimitador = obtenerCadenaSinDelimitador(cadena);
+  return cadenaSinDelimitador.split(new RegExp("[,|-]" + "|" + delimitador));
 }
 
 function sumarElementosdeCadena(elementos) {
   var resultado = 0;
   for (var iterador = 0; iterador < elementos.length; iterador++) {
       if (!isNaN(elementos[iterador])) {
-        if (parseInt(elementos[iterador]) <= 1000) {
-          resultado += parseInt(elementos[iterador]);
-        }
-
-      } else {
-          var subElementos = elementos[iterador].split(",");
-          for (var iterador2 = 0; iterador2 < subElementos.length; iterador2++) {
-            if (parseInt(subElementos[iterador2]) <= 1000) {
-              resultado += parseInt(subElementos[iterador2]);
-            }
+          if (parseInt(elementos[iterador]) <= 1000) {
+              resultado += parseInt(elementos[iterador]);
           }
+      } else {
+          resultado += sumarSubElementos(elementos[iterador]);
       }
   }
   return resultado;
+}
+
+function sumarSubElementos(subElementos) {
+  var resultadoSub = 0;
+  var subElementosSeparados = subElementos.split(",");
+  for (var iterador2 = 0; iterador2 < subElementosSeparados.length; iterador2++) {
+      if (parseInt(subElementosSeparados[iterador2]) <= 1000) {
+          resultadoSub += parseInt(subElementosSeparados[iterador2]);
+      }
+  }
+  return resultadoSub;
 }
 
 
--- src/calculadorSumaDeCadena.test.js
+++ src/calculadorSumaDeCadena.test.js
@@ -2,7 +2,7 @@ import calculadorSumaDeCadena from "./calculadorSumaDeCadena.js";
 
 describe("calculadorSumaDeCadena", () => {
   it("Deberia retornar nada cuando no se ingresa nada en el calculadorSumaDeCadena", () => {
-    expect(calculadorSumaDeCadena("")).toEqual("No se puso nada en la cadena");
+    expect(calculadorSumaDeCadena("")).toEqual(0);
   });
   it("Deberia devolver la suma de la cadena y deberia reconocer al poner con , la cadena", () => {
     expect(calculadorSumaDeCadena("10,20")).toEqual(30);
